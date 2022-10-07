113ddf5e189f447ecc7920c3b779b59443130b61
Глебов Евгений Андреевич <EGlebov@synergy.ru>
2022-10-07 14:53:29 +0300

    feat(components): старт

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   debug: true,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'marketplace-vue',
+    title: 'test',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -16,35 +16,17 @@ export default {
     link: [
       { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicons/favicon.ico' },
       { rel: 'icon', type: 'image/png', href: '/favicon.png' },
-      {
-        rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png',
-      },
-      {
-        rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png', type: 'image/png',
-      },
-      { rel: 'apple-touch-icon-precomposed', href: '/apple-touch-icon-precomposed.png' },
-      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
-      { rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-touch-icon-57x57.png' },
-      { rel: 'apple-touch-icon', sizes: '60x60', href: '/apple-touch-icon-60x60.png' },
-      { rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-touch-icon-72x72.png' },
-      { rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-touch-icon-76x76.png' },
-      { rel: 'apple-touch-icon', sizes: '114x114', href: '/apple-touch-icon-114x114.png' },
-      { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-touch-icon-120x120.png' },
-      { rel: 'apple-touch-icon', sizes: '144x144', href: '/apple-touch-icon-144x144.png' },
-      { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-touch-icon-152x152.png' },
-      { rel: 'apple-touch-icon', sizes: '167x167', href: '/apple-touch-icon-167x167.png' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon-180x180.png' },
-      { rel: 'apple-touch-icon', sizes: '1024x102', href: '/apple-touch-icon-1024x1024.png' },
     ],
   },
-
+  router: {
+    base: '/nuxt/',
+  },
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['./assets/styles/main.scss', 'swiper/css/swiper.min.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '@/plugins/vue-awesome-swiper', mode: 'client' },
-    { src: '@/plugins/lander.js', mode: 'client' },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
