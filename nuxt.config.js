322a0e2b9193624d5166ccb3b25f316359fae869
Глебов Евгений Андреевич <EGlebov@synergy.ru>
2022-10-07 10:25:29 +0300

    feat(components): старт

---
+++ nuxt.config.js
@@ -0,0 +1,119 @@
+process.env.DEBUG = 'nuxt:*';
+
+export default {
+  debug: true,
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'marketplace-vue',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { hid: 'keywords', name: 'keywords', content: '' },
+      { name: 'theme-color', content: '#fff' },
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+    ],
+    link: [
+      { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicons/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+      {
+        rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png',
+      },
+      {
+        rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png', type: 'image/png',
+      },
+      { rel: 'apple-touch-icon-precomposed', href: '/apple-touch-icon-precomposed.png' },
+      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      { rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-touch-icon-57x57.png' },
+      { rel: 'apple-touch-icon', sizes: '60x60', href: '/apple-touch-icon-60x60.png' },
+      { rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-touch-icon-72x72.png' },
+      { rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-touch-icon-76x76.png' },
+      { rel: 'apple-touch-icon', sizes: '114x114', href: '/apple-touch-icon-114x114.png' },
+      { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-touch-icon-120x120.png' },
+      { rel: 'apple-touch-icon', sizes: '144x144', href: '/apple-touch-icon-144x144.png' },
+      { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-touch-icon-152x152.png' },
+      { rel: 'apple-touch-icon', sizes: '167x167', href: '/apple-touch-icon-167x167.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon-180x180.png' },
+      { rel: 'apple-touch-icon', sizes: '1024x102', href: '/apple-touch-icon-1024x1024.png' },
+    ],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['./assets/styles/main.scss', 'swiper/css/swiper.min.css'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    { src: '@/plugins/vue-awesome-swiper', mode: 'client' },
+    { src: '@/plugins/lander.js', mode: 'client' },
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://github.com/nuxt-community/robots-module
+    '@nuxtjs/robots',
+    // https://github.com/nuxt-community/gtm-module
+    '@nuxtjs/gtm',
+    // https://www.npmjs.com/package/nuxt-facebook-pixel-module
+    'nuxt-facebook-pixel-module',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+
+  axios: {
+    debug: false,
+  },
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'ru',
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+
+  env: {
+    NUXT_ENV_S3BACKET: process.env.NUXT_ENV_S3BACKET,
+    SITE_FB_PIXEL: process.env.SITE_FB_PIXEL,
+    SITE_GTM: process.env.SITE_GTM,
+  },
+
+  publicRuntimeConfig: {
+    SITE_URL: process.env.SITE_URL,
+  },
+
+  // robots.txt
+  robots: {
+    UserAgent: '*',
+    Disallow: '/admin',
+    Allow: '*',
+  },
+
+  // GTM
+  gtm: {
+    id: process.env.SITE_GTM,
+  },
+
+  // Facebook pixel
+  facebook: {
+    /* module options */
+    track: 'PageView',
+    pixelId: process.env.SITE_FB_PIXEL,
+    autoPageView: true,
+    disabled: false,
+  },
+};
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">Тест</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
